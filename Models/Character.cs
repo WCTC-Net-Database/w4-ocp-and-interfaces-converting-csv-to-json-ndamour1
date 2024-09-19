8b7d360dbad851e4de82b97725b5c354d83a4091
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-19 18:58:50 -0400

    Update Character.cs
    Assignment Character class file code.

--- Models/Character.cs
+++ Models/Character.cs
@@ -2,9 +2,33 @@ namespace W4_assignment_template.Models;
 
 public class Character
 {
-    public string Name { get; set; }
-    public string Class { get; set; }
-    public int Level { get; set; }
-    public int HP { get; set; }
-    public List<string> Equipment { get; set; }
+    public string name { get; set; }
+    public string characterClass { get; set; }
+    public int level { get; set; }
+    public int hitPoints { get; set; }
+    public string[] equipment { get; set; }
+
+    public override string ToString()
+    {
+        if (name.Contains(", "))
+        {
+            int commaIndex = name.IndexOf(',');
+            string firstName = name.Substring(commaIndex + 2);
+            string lastName = name.Substring(0, commaIndex);
+            name = $"{firstName} {lastName}";
+        }
+        string characterText = $"{name} the {characterClass}\nLevel: {level}\nHP: {hitPoints}\nEquipment: ";
+        for (int i = 0; i < equipment.Length; i++)
+        {
+            if (i != equipment.Length - 1)
+            {
+                characterText += $"{equipment[i]}, ";
+            }
+            else
+            {
+                characterText += $"{equipment[i]}\n";
+            }
+        }
+        return characterText;
+    }
 }
