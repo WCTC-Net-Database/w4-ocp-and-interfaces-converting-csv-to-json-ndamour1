f88f3bb20ca4070cd46bd8c9da9186a6a9f01d62
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-19 18:57:53 -0400

    Additional Assignment Interface Files

---
+++ Interfaces/IInput.cs
@@ -0,0 +1,8 @@
+using CharacterConsole;
+
+namespace W4_assignment_template.Interfaces;
+
+public interface IInput
+{
+    string ReadLine();
+}
---
+++ Interfaces/IOutput.cs
@@ -0,0 +1,9 @@
+using CharacterConsole;
+
+namespace W4_assignment_template.Interfaces;
+
+public interface IOutput
+{
+    void WriteLine(string message);
+    void Write(string message);
+}
